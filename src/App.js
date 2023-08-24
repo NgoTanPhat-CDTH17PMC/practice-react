8f9df4048d5fd61abfe1225d2b6836cea38bcb57
Tan Phat <56815516+NgoTanPhat-CDTH17PMC@users.noreply.github.com>
2023-08-24 21:47:02 +0700

    render table list user

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from "./logo.svg";
 import "./App.scss";
 import Header from "./components/Header";
 import TableUser from "./components/TableUser";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <TableUser />
+      <Container>
+        <TableUser />
+      </Container>
     </div>
   );
 }
--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,4 +1,53 @@
+import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import axios from "axios";
+import { fetchAllUser } from "../services/UserService";
+import Button from "react-bootstrap/Button";
 const TableUser = (props) => {
-  return <div className="table-container">Table</div>;
+  const [listUser, setListUser] = useState([]);
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    let res = await fetchAllUser();
+    if (res && res.data) {
+      setListUser(res.data.data);
+    } else {
+    }
+  };
+  return (
+    <>
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUser &&
+            listUser.length > 0 &&
+            listUser.map((item, index) => {
+              return (
+                <tr key={`user-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>
+                    <Button variant="warning mx-3">Edit</Button>
+                    <Button variant="danger">Delete</Button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+    </>
+  );
 };
 export default TableUser;
